e21b50226953b65ca1f649354637644bdf74c45b
irwanphan <irwanphan@gmail.com>
2021-08-17 16:22:40 +0700

    restructured old handlers to NotesServices

--- src/api/notes/routes.js
+++ src/api/notes/routes.js
@@ -5,47 +5,49 @@ const { addNoteHandler,
     deleteNoteByIdHandler 
 } = require("./handler");
 
-const routes = [
-    {
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-            return 'Homepage';
-        },
-    },
-    {
-        method: 'POST',
-        path: '/notes',
-        // handler: (request, h) => {
-        //     return 'save a note';
-        // },
-        handler: addNoteHandler,
-        options: {
-            cors: {
-                origin: ['*']
-            }
-        }
-    },
-    {
-        method: 'GET',
-        path: '/notes',
-        handler: getAllNotesHandler,
-    },
-    {
-        method: 'GET',
-        path: '/notes/{id}',
-        handler: getNoteByIdHandler,
-    },
-    {
-        method: 'PUT',
-        path: '/notes/{id}',
-        handler: editNoteByIdHandler,
-    },
-    {
-        method: 'DELETE',
-        path: '/notes/{id}',
-        handler: deleteNoteByIdHandler,
-    },
-]
+// const routes = [
+//     {
+//         method: 'GET',
+//         path: '/',
+//         handler: (request, h) => {
+//             return 'Homepage';
+//         },
+//     },
+//     {
+//         method: 'POST',
+//         path: '/notes',
+//         // handler: (request, h) => {
+//         //     return 'save a note';
+//         // },
+//         handler: addNoteHandler,
+//         options: {
+//             cors: {
+//                 origin: ['*']
+//             }
+//         }
+//     },
+//     {
+//         method: 'GET',
+//         path: '/notes',
+//         handler: getAllNotesHandler,
+//     },
+//     {
+//         method: 'GET',
+//         path: '/notes/{id}',
+//         handler: getNoteByIdHandler,
+//     },
+//     {
+//         method: 'PUT',
+//         path: '/notes/{id}',
+//         handler: editNoteByIdHandler,
+//     },
+//     {
+//         method: 'DELETE',
+//         path: '/notes/{id}',
+//         handler: deleteNoteByIdHandler,
+//     },
+// ]
+
+const routes = () => {}
 
 module.exports = routes
---
+++ src/services/inMemory/NotesServices.js
@@ -0,0 +1,82 @@
+const { nanoid } = require('nanoid')
+
+class NotesService {
+    constructor() {
+        this._notes = []
+    }
+
+    addNote({ title, body, tags }) {
+        const id = nanoid(16)
+        const createdAt = new Date().toISOString()
+        const updatedAt = createdAt
+
+        const newNote = {
+            title, tags, body, id, createdAt, updatedAt
+        }
+
+        this._notes.push(newNote)
+
+        const isSuccess = this._notes.filter((note) => note.id === id).length > 0
+
+        if (!isSuccess) {
+            throw new Error('Catatan gagal ditambahkan')
+        }
+
+        return id
+    }
+
+    getNotes() {
+        return this._notes
+    }
+
+    getNoteById(id) {
+        const note = this._notes.filter((note) => note.id === id)[0]
+
+        // if (note !== undefined) {
+        if (!note) {
+            // return {
+            //     status: 'fail',
+            //     message: 'Catatan tidak ditemukan',
+            // }
+            throw new Error('Catatan tidak ditemukan')
+        }
+
+        // return {
+        //     status: 'success',
+        //     data: {
+        //         note,
+        //     },
+        // }
+        return note
+    }
+
+    editNoteById(id, {title, body, tags}) {
+        const index = this._notes.findIndex((note) => note.id === id)
+
+        if (index === -1) {
+            throw new Error('Gagal memperbarui catatan. Id tidak ditemukan')
+        }
+
+        const updatedAt = new Date().toISOString()
+
+        this._notes[index] = {
+            ...this._notes[index],
+            title,
+            tags,
+            body,
+            updatedAt,
+        }
+    }
+
+    deleteById(id) {
+        const index = this._notes.findIndex((note) => note.id === id)
+
+        if (index === -1) {
+            throw new Error('Gagal menghapus catatan. Id tidak ditemukan')
+        }
+
+        this._notes.splice(index, 1)
+    }
+}
+
+module.exports = NotesService
