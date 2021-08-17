2ccb6c79b94cf6bbfb93fb146f44e210ededa21e
irwanphan <irwanphan@gmail.com>
2021-08-17 16:28:00 +0700

    edited routes to function that returns an array

--- src/api/notes/routes.js
+++ src/api/notes/routes.js
@@ -5,49 +5,39 @@ const { addNoteHandler,
     deleteNoteByIdHandler 
 } = require("./handler");
 
-// const routes = [
-//     {
-//         method: 'GET',
-//         path: '/',
-//         handler: (request, h) => {
-//             return 'Homepage';
-//         },
-//     },
-//     {
-//         method: 'POST',
-//         path: '/notes',
-//         // handler: (request, h) => {
-//         //     return 'save a note';
-//         // },
-//         handler: addNoteHandler,
-//         options: {
-//             cors: {
-//                 origin: ['*']
-//             }
-//         }
-//     },
-//     {
-//         method: 'GET',
-//         path: '/notes',
-//         handler: getAllNotesHandler,
-//     },
-//     {
-//         method: 'GET',
-//         path: '/notes/{id}',
-//         handler: getNoteByIdHandler,
-//     },
-//     {
-//         method: 'PUT',
-//         path: '/notes/{id}',
-//         handler: editNoteByIdHandler,
-//     },
-//     {
-//         method: 'DELETE',
-//         path: '/notes/{id}',
-//         handler: deleteNoteByIdHandler,
-//     },
-// ]
-
-const routes = () => {}
+const routes = (handler) => [
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Homepage';
+        },
+    },
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: addNoteHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNoteByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: editNoteByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: deleteNoteByIdHandler,
+    },
+]
 
 module.exports = routes
