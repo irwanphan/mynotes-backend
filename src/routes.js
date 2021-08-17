b49c41a7aa8e25efa3f6e554663cb641e1622821
irwanphan <irwanphan@gmail.com>
2021-08-17 12:16:29 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,51 @@
+const { addNoteHandler,
+    getAllNotesHandler, 
+    getNoteByIdHandler, 
+    editNoteByIdHandler,
+    deleteNoteByIdHandler 
+} = require("./handler");
+
+const routes = [
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Homepage';
+        },
+    },
+    {
+        method: 'POST',
+        path: '/notes',
+        // handler: (request, h) => {
+        //     return 'save a note';
+        // },
+        handler: addNoteHandler,
+        options: {
+            cors: {
+                origin: ['*']
+            }
+        }
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNoteByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: editNoteByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: deleteNoteByIdHandler,
+    },
+]
+
+module.exports = routes
