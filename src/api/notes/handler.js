d8f06bb5aaa2c3992d1e2b0e4d30bd0195fae436
irwanphan <irwanphan@gmail.com>
2021-08-19 17:45:11 +0700

    postNoteHandlerr

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -3,9 +3,32 @@ class NotesHandler {
         this._service = service
     }
 
-    postNoteHandler() {
+    postNoteHandler(request) {
+        try {
+            const { title = 'untitled', body, tags } = request.payload
+            // addNote returns id
+            const noteId = this._service.addNote({ title, body, tags })
+
+            const response = h.response({
+                status: 'success',
+                message: 'Catatan berhasil ditambahkan',
+                data: {
+                    noteId,
+                },
+            })
+            response.code(201)
+            return response
+        } catch (error) {
+            const response = h.response({
+                status: 'fail',
+                message: error.message,
+            })
+            response.code(400)
+            return response
+        }
     }
     getNotesHandler() {
+
     }
     getNoteByIdHandler() {
     }
